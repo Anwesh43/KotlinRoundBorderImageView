cb3e961e34fbf0f4baa2ea1f29c359ec83a29940
Anwesh43 <anweshthecool0@gmail.com>
2018-03-28 07:30:19 +0530

    adding Runner class

--- roundborderimageview/src/main/java/ui/anwesome/com/roundborderimageview/AnimatorQueue.kt
+++ roundborderimageview/src/main/java/ui/anwesome/com/roundborderimageview/AnimatorQueue.kt
@@ -6,23 +6,56 @@ import java.util.concurrent.ConcurrentLinkedQueue
 /**
  * Created by anweshmishra on 28/03/18.
  */
+val INTERVAL = 50L
 class AnimatorQueue {
-    var views : ConcurrentLinkedQueue<RoundBorderImageView> = ConcurrentLinkedQueue()
-    fun addView(view : RoundBorderImageView) {
-        views.add(view)
+
+    companion object {
+        private val animatorQueue = AnimatorQueue()
+        fun getInstance() : AnimatorQueue = animatorQueue
     }
-    var updatecb : () -> Unit =  {
-        views.forEach { view ->
-            view.update {
-                views.remove(view)
-                if (views.size == 0) {
+    data class Runner(var i : Int, var updatecb : () -> Unit) : Runnable {
+        var running : Boolean = true
+        var paused : Boolean = false
+        var views : ConcurrentLinkedQueue<RoundBorderImageView> = ConcurrentLinkedQueue()
+        fun addView(view : RoundBorderImageView, startcb : () -> Unit) {
+            views.add(view)
+            if (!running) {
+                running = true
+                startcb()
+            }
+        }
+        fun updatecb() {
+            views.forEach { view ->
+                view.update {
+                    views.remove(view)
+                    if (views.size == 0) {
+                        running = false
+                    }
+                }
+            }
+        }
+        fun pause() {
+            if (running) {
+                running = false
+                paused = true
+            }
+        }
+        fun resume(startcb : () -> Unit) {
+            if (!running && paused) {
+                paused = false
+                running = true
+            }
+        }
+        override fun run() {
+            while (running) {
+                try {
+                    Thread.sleep(INTERVAL)
+                    updatecb()
+                }
+                catch (ex : Exception) {
 
                 }
             }
         }
-    }
-    companion object {
-        private val animatorQueue = AnimatorQueue()
-        fun getInstance() : AnimatorQueue = animatorQueue
     }
 }
