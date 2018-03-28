0c8fadc8911a5e9bc8ae026ebe6a9c6d74079b82
Anwesh43 <anweshthecool0@gmail.com>
2018-03-28 07:14:32 +0530

    adding State class

--- roundborderimageview/src/main/java/ui/anwesome/com/roundborderimageview/RoundBorderImageView.kt
+++ roundborderimageview/src/main/java/ui/anwesome/com/roundborderimageview/RoundBorderImageView.kt
@@ -6,7 +6,7 @@ package ui.anwesome.com.roundborderimageview
 import android.content.*
 import android.graphics.*
 import android.view.*
-class RoundBorderImageView(ctx : Context) : View(ctx) {
+class RoundBorderImageView(ctx : Context, var bitmap : Bitmap) : View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas : Canvas) {
 
@@ -18,5 +18,22 @@ class RoundBorderImageView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var scale : Float = 0f) {
+        fun update(stopcb : (Float) -> Unit) {
+            scale += 0.1f * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                stopcb(scale)
+            }
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
     }
 }
